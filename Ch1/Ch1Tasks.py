769223a576033209595fd04693a87d84a1795e29
Michael Foy <mfoy@internal.salesforce.com>
2018-04-10 22:19:34 -0500

    Ch1 Tasks (03 Apr)

---
+++ Ch1/Ch1Tasks.py
@@ -0,0 +1,73 @@
+# Task 1.4.1
+from plotting import plot
+S={2+2j,3+2j,1.75+1j, 2+1j, 2.25+1j, 2.5+1j, 2.75+1j, 3+1j, 3.25+1j}
+plot(S,4)
+
+
+# Task 1.4.3
+plot({1+2j+z for z in S},4)
+
+
+# Quiz 1.4.4: For what value of z0 does the translation f(z) = z0 + z move the 
+# left eye to the origin?
+plot({-2-2j+z for z in S},4)
+
+
+# Problem 1.4.5: Show that, for any two distinct points z1 and z2:
+# * there is a translation that maps z1 to z2
+# * there is a translation that maps z2 to z1, and
+# * there is no translation that both maps z2 to z1 and z1 to z2
+'''
+z1 = a+bj
+z2 = c+cj
+
+z1 + X1 = z2
+X1 = z2 - z1
+X1 = (c-a)+(c-b)j
+
+
+z2 + X2 = z1
+X2 = z1 - z2
+X2= (a-c)+(b-c)j
+
+X1 != X2
+'''
+
+
+# Problem 1.4.6: Draw adiagram representing the complex number z0 = -3+3j using 
+# two arrows with their tails at different points.
+'''
+This would be a drawing of a vector <-3,3> with the endpoint at two different 
+points. For example one at (0,0), another at (1,1)
+'''
+
+
+# Task 1.4.7: Create a new plot title "My Scaled Points" using a comprehension
+# as in Task 1.4.3. The points in the new plot should be halves of the points 
+# in S.
+plot({0.5*z for z in S}, 4)
+
+
+# Task 1.4.8: Create a new plot in which the points of S are rotated by 90 
+# degrees and scaled by 1/2. Use a comprehension in which the points of S are 
+# multipled by a single complex number.
+plot({0.5j*z for z in S},4)
+
+
+
+# Task 1.4.9: Using a comprehension, create a new plot in which the points of 
+# S are rotated by 90 degrees, scaled by 1/2, and then shifted down by one 
+# unit and to the right two units. Use a comprehension in which the points of S
+# are multiple by one complex number and added to another.
+plot({0.5j*z+2-1j for z in S},4)
+
+
+# Task 1.4.10: *Import test image using provided procedure.* Use a 
+# comprehension to assign to a list pts the set of complex numbers x+yi such 
+# that the image intensity of pixel (x,y) is less than 120, and plot the list 
+# pts.
+import image
+data = image.file2image('img01.png')
+bw_data = image.color2gray(data)
+
+pts = {a[b]+b*j for a in bw_data for b in bw_data[0] if a[b] < 120}
