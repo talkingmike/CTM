2345337b8e4053f76df2753f3608f112d0f51d2a
Michael Foy <mfoy@internal.salesforce.com>
2018-05-03 23:37:39 -0500

    Ch1 Tasks complete

--- Ch1/Ch1Tasks.py
+++ Ch1/Ch1Tasks.py
@@ -1,3 +1,4 @@
+
 from plotting import plot
 
 # Task 1.4.1
@@ -67,10 +68,6 @@ plot({0.5j*z+2-1j for z in S},4)
 # comprehension to assign to a list pts the set of complex numbers x+yi such 
 # that the image intensity of pixel (x,y) is less than 120, and plot the list 
 # pts.
-import image
-data = image.file2image('img01.png')
-bw_data = image.color2gray(data)
-
 '''
     Keep these in mind:
     Something to do with this statement: [[x for x in row] for row in image]
@@ -81,7 +78,64 @@ bw_data = image.color2gray(data)
     Use enumerate(List) to return a tuple of (index,List(item))
 '''
 
-pts = [complex(col_ind,row_ind) for (row_ind,row) in enumerate(bw_data) for 
+import image
+data = image.file2image('img01.png')
+bw_data = image.color2gray(data)
+
+pts = {complex(col_ind,189-row_ind) for (row_ind,row) in enumerate(bw_data) for 
+       (col_ind,intensity) in enumerate(row) if intensity < 120}
+
+plot(pts, 200, 1)
+
+
+# Task 1.4.11: Write a Python procedure f(z) that takes as an argument a 
+# complex number z so that when f(z) applied to each of the complex numbers in
+# S, the set of resulting numbers is centered at the origin. Write a 
+# comprehension in terms of S and f whose value is the set of translated 
+# points, and plot the value.
+def func_11(z): return {(-2.5-1.5j)+i for i in z}
+
+
+
+# Task 1.4.12: Repeat Task 1.4.8 with the points in pts instead of the points
+# in S.
+def func_12(z): return {-83-95j+i for i in z}
+
+
+# Task 1.4.17: From the module math, import the definitions e and pi. Let n be 
+# the integer 20. Let w be the complex number e^(2*pi*i/n). Write a 
+# comprehension yielding the list consisting of w0, w1, w2, ..., wn-1. Plot 
+# these complex numbers.
+from math import e
+from math import pi
+
+n = 20
+w = e**(pi*2j/n)
+list_17 = [w**p for p in list(range(n))]
+plot(list_17)
+
+
+# Task 1.4.18: Recall from Task 1.4.1 the set S of complex numbers. Write a 
+# comprehension whose value is the set consisting of rotations by pi/4 of the 
+# elements of S. Plot the value of this comprehension.
+'''
+    Remember: function that rotates by tau is: f(z) = z*e**(tau*i)
+'''
+from math import pi
+
+S = {2+2j,3+2j,1.75+1j, 2+1j, 2.25+1j, 2.5+1j, 2.75+1j, 3+1j, 3.25+1j}
+
+list_18 = [e**(pi/4*1j)*p for p in S]
+
+
+# Tasks 1.4.19: Similar, recall from Task 1.4.10 the list pts of points derived 
+# from an image. Plot the rotation by pi/4 of the complex numbers comprising 
+# pts.
+import image
+data = image.file2image('img01.png')
+bw_data = image.color2gray(data)
+
+pts = [complex(col_ind,189-row_ind) for (row_ind,row) in enumerate(bw_data) for 
        (col_ind,intensity) in enumerate(row) if intensity < 120]
 
-plot({-1j*z+189j for z in pts}, 200, 1)
+list_19 = [e**(pi/4*1j)*p for p in pts]
